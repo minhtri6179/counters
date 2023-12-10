ce905afab7e3359945fcd9c22574d8d06af848ab
Tri Nguyen <minhtri.ngt@gmail.com>
2023-12-11 01:44:42 +0700

    test: env file

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: true
 });
 
 const env = process.env.NODE_ENV || 'development';
