59be0d6a8d4f87d19b10ac3fa40a5a0edf421e13
Tri Nguyen <minhtri.ngt@gmail.com>
2023-12-10 18:17:38 +0700

    test: cd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 8000;
+const host = process.env.HOST || '127.0.0.1';
 
 app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
